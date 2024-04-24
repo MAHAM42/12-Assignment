b2b22700f887233ad1c740a65be9284c9e970018
MAHAM42 <161121868+MAHAM42@users.noreply.github.com>
2024-04-24 11:24:35 -0700

    12-Assignment
    TypeScript 45-Assignment of Q12 with Node.js

---
+++ main12.js
@@ -0,0 +1,15 @@
+"use strict";
+// ASSIGNMENT NO 12:
+/*
+Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+ print a message to them. The text of each message should be the same,
+ but each message should be personalized with the person’s name.
+*/
+console.log("ASSIGNMENT NO 12:");
+let names = ["Abeera", "Hamna", "Mahnoor", "Unsa", "Zainab"];
+let message = "Do you like to play hide and seek?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
+console.log(names[4] + " " + message);
---
+++ main12.ts
@@ -0,0 +1,18 @@
+// ASSIGNMENT NO 12:
+
+/*
+Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+ print a message to them. The text of each message should be the same, 
+ but each message should be personalized with the person’s name.
+*/
+
+ console.log("ASSIGNMENT NO 12:")
+
+let names : string[] = ["Abeera","Hamna","Mahnoor","Unsa","Zainab"]
+
+ let message : string = "Do you like to play hide and seek?"
+console.log(names[0] + " "+ message )
+console.log(names[1] + " "+ message )
+console.log(names[2] + " "+ message )
+console.log(names[3] + " "+ message )
+console.log(names[4] + " "+ message )
